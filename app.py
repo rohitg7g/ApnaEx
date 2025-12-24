ab390269170f6b488a364e9686af6b920ff6206e
rohitg7g <rohitchaudhary21909@gmail.com>
2025-12-24 15:47:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,13 @@
 
 import os
-from flask import Flask, request, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify({
-        "status": "ok",
-        "message": "ApnaEx server is running",
-        "platform": "Render"
-    })
-
-@app.route("/health")
-def health():
-    return "OK", 200
-
-@app.route("/echo", methods=["POST"])
-def echo():
-    data = request.json
-    return jsonify({
-        "received": data
-    })
+    return "ApnaEx web is running on Railway"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port)
