86147985a3aa0f174528ccd60fb7e61b9994ca25
rohitg7g <rohitchaudhary21909@gmail.com>
2025-12-24 15:27:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
+
 import os
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-PORT = int(os.environ.get("PORT", 8080))
 
 @app.route("/")
 def home():
-    return "Hello, Render!"
+    return jsonify({
+        "status": "ok",
+        "message": "ApnaEx server is running",
+        "platform": "Render"
+    })
+
+@app.route("/health")
+def health():
+    return "OK", 200
+
+@app.route("/echo", methods=["POST"])
+def echo():
+    data = request.json
+    return jsonify({
+        "received": data
+    })
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=PORT)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
